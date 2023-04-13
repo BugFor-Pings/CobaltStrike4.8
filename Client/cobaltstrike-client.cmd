34106ac9617f00a0ffd8bf914f514073e477058b
Pings <96440773+BugFor-Pings@users.noreply.github.com>
2023-04-14 04:19:29 +0800

    Delete Client directory

--- Client/cobaltstrike-client.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-echo   [+] Starting cobaltstrike client
-java -XX:ParallelGCThreads=4 -XX:+AggressiveHeap -XX:+UseParallelGC -javaagent:uHook.jar -Dfile.encoding=utf-8 -jar cobaltstrike-client.jar $*
